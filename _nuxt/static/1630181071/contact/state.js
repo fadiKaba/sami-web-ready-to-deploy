d6f86fad4fc1189269156438251bb4cb07ad22af
fadiKaba <fadikaba9@gmail.com>
2021-08-29 20:41:25 +0200

    portfolio

--- _nuxt/static/1630181071/contact/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630181071",layout:"default",error:a,state:{layoutStateNave:b,layoutState:b},serverRendered:true,routePath:"\u002Fcontact",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,false));
