ca3adc97ac957b53956a8acca726e3723a73a61f
fadiKaba <fadikaba9@gmail.com>
2021-09-02 22:19:33 +0200

    texture

---
+++ _nuxt/static/1630613901/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630613901",layout:"default",error:a,state:{layoutStateNave:b,layoutState:b,runLoadingPage:c},serverRendered:c,routePath:"\u002Fabout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,false,true));
